7fc21044ff1fcfa885835f780b0761e3d0718eb7
Vinoth kumar <vinothpkumar333@gmail.com>
2019-10-29 13:41:07 +0530

    Update DMS_Audit.sh

--- DMS_Audit.sh
+++ DMS_Audit.sh
@@ -3,7 +3,7 @@
 
 for profile in `cat profiles`;do #Iterates through different accounts in your org account. 
 	awsoolist > creds_dms #Gets temporary credentials
-	for region in `AWS_SHARED_CREDENTIALS_FILE=creds aws ec2 describe-regions --profile $profile --r us-east-1 | jq -r ".Regions[].RegionName"`;do #Iterates through different regions in an account.
+	for region in `AWS_SHARED_CREDENTIALS_FILE=creds_dms aws ec2 describe-regions --profile $profile --r us-east-1 | jq -r ".Regions[].RegionName"`;do #Iterates through different regions in an account.
 		for rep_instance in `AWS_SHARED_CREDENTIALS_FILE=creds_dms aws dms describe-replication-instances --region $region --profile $profile| jq -r ".ReplicationInstances[].ReplicationInstanceArn"`;do #Iterates through different replication instance
 		#Check if AutoMinorVersionUpgrade feature is enabled or not - https://www.cloudconformity.com/knowledge-base/aws/DMS/auto-minor-version-upgrade.html
 			AutoMinorVersionUpgrade=`AWS_SHARED_CREDENTIALS_FILE=creds_dms aws dms describe-replication-instances --region $region --filters Name=replication-instance-arn,Values=$rep_instance --profile $profile | jq -r ".ReplicationInstances[].AutoMinorVersionUpgrade"`  		
